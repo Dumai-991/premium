1dfcc0b56597238d93768071ff1ddf4338265f19
Mr.Risky <santuyaja019@gmail.com>
2021-07-20 11:51:44 +0700

    first commit

---
+++ setting.py
@@ -0,0 +1,199 @@
+###########################################################################
+# TEAM   : YAYAN,ANGGA,DAPUNTA,RISKY,WANS,ITSUKI			  #
+# SOSIAL : 								  #
+# - Facebook : https://m.facebook.com/llovexnxx				  #
+# - WhatsApp : https://wa.me/6283143565470				  #
+# - TeleGram : 6283143565470       					  #
+# - Github   : https://github.com/Dumai-991n 				  #
+# INFO   :								  #
+# - Hallo Raja Recoder Boleh Edit Tapi Jangan Berlebihan Oke...           #
+# - Script Jangan DiPerjual Belikan :( Karena Anda Noob Kentod            #
+###########################################################################
+me = ("Mr.Risky")
+no_me = ("6283143565470")
+email_me = ("santuyaja019@gmail.com")
+facebook_me = ("Https://M.Facebook.Com/llovexnxx")
+github_me = ("Https://Github.Com/Dumai-991")
+team = ("Angga, Yayan, Dapunta, Mr.Risky, Wans And ITSUKI")
+url="https://free.facebook.com"
+useragent="Mozilla/5.0 (Linux; Android 5.1; PICOphone_M4U_M2_M Build/LMY47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36"
+newpass="Dumai991"
+
+#WARNA_COPY_Right
+#p = "\x1b[0;37m" # putih
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[1;94m'
+i='\033[0;92m'
+c='\033[0;96m'
+m='\033[0;91m'
+u='\033[0;95m'
+k='\033[0;93m'
+p='\033[0;97m'
+h='\033[0;90m'
+p = "\x1b[0;33m" # putih
+m = "\x1b[0;31m" # merah
+h = "\x1b[0;32m" # hijau
+k = "\033[0;36m" # kuning
+b = "\x1b[0;34m" # biru
+u = "\x1b[0;35m" # ungu
+o = "\033[0;32m" # biru muda
+bulat=(k+"["+p+"•"+k+"] "+p)   #   [•]
+war=(k+"["+p+"!"+k+"] "+p)  # [!]
+inp=(k+"["+p+"?"+k+"] "+p) # [?]
+bulat2=(k+"["+p+"••"+k+"] "+p)   # [••]
+war2=(k+"["+p+"!!"+k+"] "+p)  # [!!]
+inp2=(k+"["+p+"??"+k+"] "+p) # [??]
+garis=(k+'['+i+'+'+k+']'+p+'========================================================='+k+'['+i+'+'+k+']')
+
+import requests as r, re, os
+from bs4 import BeautifulSoup as par
+import platform
+import requests, bs4, sys, os, subprocess, random, time, re, json
+import concurrent.futures
+from datetime import datetime
+from time import sleep
+from requests import Session
+import re, sys
+import sys
+from os import system
+import os, sys, time, random
+from sys import exit as keluar
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from sys import stdout
+from os import system
+import re
+import os,random,time,sys
+import json
+try: import requests
+except ModuleNotFoundError: os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError: os.system("python -m pip install bs4 &> /dev/null")
+import requests as req
+from time import sleep as waktu
+from bs4 import BeautifulSoup as parser
+current = datetime.now()
+import requests,mechanize,bs4,sys,os,subprocess,uuid,random,time,re,base64,concurrent.futures,json
+koneksi_error=(req.exceptions.ConnectionError,req.exceptions.ChunkedEncodingError,req.exceptions.ReadTimeout)
+#Jaga Jarak Kawan :v
+ua = {"user-agent":"chrome"}
+prvt = []
+ses = r.Session()
+link = "https://free.facebook.com/"
+#Memek Kau :p
+N="\033[00m"
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[0;34m'
+i='\033[0;32m'
+c='\033[0;36m'
+m='\033[0;31m'
+u='\033[0;35m'
+k='\033[0;33m'
+p='\033[0;37m'
+h='\033[0;90m'
+b='\033[0;34m'
+I='\033[0;32m'
+C='\033[0;36m'
+M='\033[0;31m'
+U='\033[0;35m'
+K='\033[0;33m'
+P='\033[0;37m'
+H='\033[0;90m'
+warna_me=([i, c, m, u, k, p, h, b])
+warna_ms=([i, c, m, u, k, p, h, b])
+w2=(warna_me)
+w=random.choice(w2)
+try:
+	kntlx=([i, c, m, u, k, p, h, b])
+	for w3 in kntlx:
+		continue
+except KeyError:
+	w3 = ("")
+
+def banner():
+	logo_v2=(f"""
+    ____                               _                     \n   / __ \   _____  ___    ____ ___    (_)  __  __   ____ ___ \n  / /_/ /  / ___/ / _ \  / __ `__ \  / /  / / / /  / __ `__ \ \n / ____/  / /    /  __/ / / / / / / / /  / /_/ /  / / / / / /\n/_/      /_/     \___/ /_/ /_/ /_/ /_/   \__,_/  /_/ /_/ /_/ 
+{bulat}Author   : {i}{me}
+{bulat}Github   : {i}{github_me}
+{bulat}Whatsapp : {i}https://wa.me/{no_me}
+{bulat}Facebook : {i}https://fb.me/llovenxx
+{war}{q}Thanks To : {i}{team}\n{garis}""")
+	logo_v1=(f"""
+________                           _____                     \n___  __ \_____________ _______ ___ ___(_)____  _________ ___ \n__  /_/ /__  ___/_  _ \__  __ `__ \__  / _  / / /__  __ `__ \ \n_  ____/ _  /    /  __/_  / / / / /_  /  / /_/ / _  / / / / /\n/_/      /_/     \___/ /_/ /_/ /_/ /_/   \__,_/  /_/ /_/ /_/ 
+{bulat}Author   : {i}{me}
+{bulat}Github   : {i}{github_me}
+{bulat}Whatsapp : {i}https://wa.me/{no_me}
+{bulat}Facebook : {i}https://fb.me/llovenxx
+{war}{q}Thanks To : {i}{team}\n{garis}""")
+	rizky = ([logo_v1, logo_v2])
+	ganteng = pilih(rizky)
+	print(w3+ganteng)
+
+host="https://mbasic.facebook.com"
+ips=None
+try:
+        ipx=requests.get("http://ip-api.com/json/").json()["query"]
+        ips=requests.get("http://ip-api.com/json/"+ipx,headers={"Referer":"http://ip-api.com/","Content-Type":"application/json; charset=utf-8","User-Agent":"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"}).json()["country"].lower()
+except:
+        ips=None
+
+
+def kntl():
+#                 %s"[ K N T L ]"
+        lpp___1 =("%s[          "%(k))
+        lpp___2 =("%s[ %sK        "%(k, m))
+        lpp___3 =("%s[ %sK %sN      "%(k, m, u))
+        lpp___4 =("%s[ %sK %sN %sT    "%(k, m, u, c))
+        lpp___5 =("%s[ %sK %sN %sT %sL "%(k, m, u, c, b))
+        lpp___6 =("%s[ %sK %sN %sT %sL %s]"%(k, m, u, c, b, k))
+        lpp___7 =("%s[ %sK %sN %sT %sL %s]"%(k, m, u, c, b, k))
+        lpp___8 =("%s[ %sK %sN %sT %sL "%(k, m, u, c, b))
+        lpp___9 =("%s[ %sK %sN %sT    "%(k, m, u, c))
+        lpp___10 =("%s[ %sK %sN      "%(k, m, u))
+        lpp___11 =("%s[ %sK        "%(k, m))
+        lpp___12 =("%s           "%(k))
+        lpp___13 =("%s[         ]"%(k))
+        lpp___14 =("  %sK %sN %sT %sL  "%(m, u, c, b,))
+        lpp___15 =("%s[          "%(k))
+        lpp___16 =("%s          ]"%(k))
+        lpp___17 =("%s[ %sD %sO %sN %sE %s]"%(k, i, i, i, i, k))
+
+        lpp___all = ([lpp___1, lpp___2, lpp___3, lpp___4, lpp___5, lpp___6,lpp___7, lpp___8, lpp___9,lpp___10, lpp___11, lpp___12, lpp___13,lpp___14, lpp___13, lpp___14, lpp___13, lpp___14, lpp___13, lpp___14, lpp___15, lpp___16, lpp___17])
+        for x in lpp___all:
+            stdout.write(p+'\r['+w3+'XXXX'+p+']'+k+'=='+u+'> '+x)
+            stdout.flush(),;time.sleep(0.30)
+def logo_mt():
+#### Hai Kak :-/
+	dmi_1 = (f"""{c}   __  ___         _         __
+  /  |/  / ___ _  (_)  ___  / /_ ___   ___  ___ _  ___  ____ ___ 
+ / /|_/ / / _ `/ / /  / _ \/ __// -_) / _ \/ _ `/ / _ \/ __// -_)
+/_/  /_/  \_,_/ /_/  /_//_/\__/ \__/ /_//_/\_,_/ /_//_/\__/ \__/ 
+{bulat}Under maintenance, please try for a while
+{bulat}POWER OF DUMAI-991
+{war}HUB : 6283143565470""")
+	print(dmi_1)
+
+def logo_exp():
+	dmi_2 = (f"""{c} _______ _     _  _____  _____  ______ _______ ______ \n |______  \___/  |_____]   |   |_____/ |______ |     \ \n |______ _/   \_ |       __|__ |    \_ |______ |_____/
+ {war}Maaf Script Ini Sudah Expired Silahkan Hubungi Admin !!
+ {war}Whatsapp : {k}{no_me}
+ {war}Facebook : {k}{facebook_me}""")
+	print(dmi_2)
+####   Hai anak recoder :)
